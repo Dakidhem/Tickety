0ac686b48d53bc8e9dd8cf3aeb1bec2666928b6a
[MehdiSfizer] <[mehdi@sfizer.consulting]>
2023-12-04 23:32:45 +0100

    add Notifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,9 +90,10 @@ const Navbar = () => {
             id="navbar-cta"
           >
             <div className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <NotificationsModal />
               <a
                 to="/dashboard"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-center"
               >
                 Dashboard
               </a>
